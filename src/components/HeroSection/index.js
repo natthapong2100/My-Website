f06d2cef4f72695e93ca0d2794de3c0175626343
Natthapong Lueangphumphitthaya <nat@Nat-MacBook-Pro.local>
2022-01-20 01:05:26 +0700

    third commit: add InfoSection

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import Video from '../../videos/video.mp4';
+import { Button } from '../ButtonElement';
+import { HeroContainer, HeroBg, VideoBg, HeroContent, 
+    HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements';
+
+const HeroSection = () => {
+
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover);
+
+    };
+
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+            </HeroBg>
+
+            <HeroContent>
+                <HeroH1>
+                    Natthapong Lueangphumphitthaya
+                </HeroH1>
+                <HeroP>
+                    Welcome to My Website
+                </HeroP>
+
+                <HeroBtnWrapper>
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}
+                    primary="true" dark="true">
+                        Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+            
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,21 @@
+import { nativeTouchData } from "react-dom/cjs/react-dom-test-utils.production.min";
+
+import natPic from '../../images/nat-pic.JPG'
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'About Me',
+    headline: 'Software Engineering Student at KMITL',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'My Resume',
+    imgStart: false,
+    img: natPic,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    dartText: false
+
+};
