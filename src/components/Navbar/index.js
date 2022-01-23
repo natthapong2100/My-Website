2ef1129f845049b50b292a2489715c7ed2d52e92
Natthapong Lueangphumphitthaya <nat@Nat-MacBook-Pro.local>
2022-01-23 16:07:29 +0700

    try header

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -31,7 +31,7 @@ const Navbar = ({toggle}) => {
                 <Nav scrollNav={scrollNav}>
                     <NavbarContainer>
                         <NavLogo to='/' onClick={toggleHome}>
-                            Natthapong L.
+                            Natthapong L. XXXX
                         </NavLogo>
                         <MobileIcon onClick={toggle} >
                             <FaBars />
